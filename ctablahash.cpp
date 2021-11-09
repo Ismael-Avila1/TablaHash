3f44604488db8c1a83e4b6e5a86c0c11db7a8862
Ismael Avila <ismael.guada234015@gmail.com>
2021-11-09 13:45:12 -0600

    Definición e implementación de cTablaHash

--- ctablahash.cpp
+++ ctablahash.cpp
@@ -1,6 +1,42 @@
 #include "ctablahash.h"
 
-cTablaHash::cTablaHash()
-{
+cTablaHash::cTablaHash() {}
 
+int cTablaHash::fnHash(std::string nombre, std::string estado, int temp) {
+    int index;
+
+    index = nombre.length() + estado.length();
+    index = index*temp;
+
+    index = index % SIZE;
+
+    return index;
+}
+
+void cTablaHash::insertar(cInfoPueblo *info) {
+    int index = fnHash(info->nombre, info->estado, info->tempMedia);
+
+    arr[index].insertar(info);
+}
+
+cInfoPueblo* cTablaHash::buscar(std::string nombre, std::string estado, int temp) {
+    int index = fnHash(nombre, estado, temp);
+
+    return arr[index].buscar(nombre);
+}
+
+void cTablaHash::eliminar(std::string nombre, std::string estado, int temp) {
+    int index = fnHash(nombre, estado, temp);
+
+    arr[index].eliminar(nombre);
+}
+
+void cTablaHash::imprimir() {
+    for(int i=0; i<SIZE; i++)
+        arr[i].imprimir();
+}
+
+void cTablaHash::serializar(std::fstream &out) {
+    for(int i=0; i<SIZE; i++)
+        arr[i].serializar(out);
 }
--- ctablahash.h
+++ ctablahash.h
@@ -1,11 +1,25 @@
 #ifndef CTABLAHASH_H
 #define CTABLAHASH_H
 
+#include "clista.h"
 
-class cTablaHash
-{
+#define SIZE 17
+
+class cTablaHash {
+private:
+    cLista arr[SIZE];
+
+    int fnHash(std::string nombre, std::string estado, int temp);
+
 public:
     cTablaHash();
+
+    void insertar(cInfoPueblo* info);
+    cInfoPueblo* buscar(std::string nombre, std::string estado, int temp);
+    void eliminar(std::string nombre, std::string estado, int temp);
+
+    void imprimir();
+    void serializar(std::fstream& out);
 };
 
 #endif // CTABLAHASH_H
