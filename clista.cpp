673f7070c5d1008a77e742fec2b28943f81f195b
Ismael Avila <ismael.guada234015@gmail.com>
2021-11-09 13:21:24 -0600

    Definición e implementación de la cLista

--- clista.cpp
+++ clista.cpp
@@ -1,6 +1,91 @@
 #include "clista.h"
 
-cLista::cLista()
-{
+cLista::cLista() {
+    inicio.pSig = &final;
+    final.pAnt = &inicio;
+}
 
+cLista::~cLista() {
+    cNodo* temp;
+
+    while (inicio.pSig != &final) {
+        temp = inicio.pSig;
+        inicio.pSig = inicio.pSig->pSig;
+
+        delete temp;
+    }
+    final.pAnt = &inicio;
+}
+
+void cLista::insertar(cInfoPueblo *info) {
+    final.pAnt->insertarAdelante(info);
+}
+
+cInfoPueblo *cLista::eliminar(std::string nombrePueblo) {
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++)
+        if(it.pActual->infoNodo->nombre == nombrePueblo)
+            return it.pActual->pAnt->eliminarAdelante();
+
+    return nullptr;
+}
+
+cInfoPueblo *cLista::buscar(std::string nombrePueblo) {
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++)
+        if(it.pActual->infoNodo->nombre == nombrePueblo)
+            return it.pActual->infoNodo;
+
+    return nullptr;
+}
+
+int cLista::size() {
+    int contNodes = 0;
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++)
+        contNodes++;
+
+    return contNodes;
+}
+
+bool cLista::isEmpty() {
+    return inicio.pSig == &final;
+}
+
+bool cLista::isFull() {
+    return false;
+}
+
+void cLista::imprimir() {
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++) {
+        it.pActual->infoNodo->imprimir();
+        std::cout << std::endl << std::endl;
+    }
+}
+
+cIterador &cLista::begin() {
+    itBegin.setNodo(inicio.pSig);
+
+    return itBegin;
+}
+
+cIterador &cLista::end() {
+    itEnd.setNodo(&final);
+
+    return itEnd;
+}
+
+void cLista::serializar(std::fstream &out) {
+    out << CLSID_LISTA << std::endl;
+    out << size() << std::endl;
+
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++)
+        it.pActual->infoNodo->serializar(out);
 }
--- clista.h
+++ clista.h
@@ -1,11 +1,40 @@
 #ifndef CLISTA_H
 #define CLISTA_H
 
+#include <fstream>
 
-class cLista
-{
+#include "cnodo.h"
+#include "citerador.h"
+
+#define CLSID_LISTA 1000
+
+class cLista {
+private:
+    cNodo inicio;
+    cNodo final;
+
+    cIterador itBegin;
+    cIterador itEnd;
+
 public:
     cLista();
+    ~cLista();
+
+    void insertar(cInfoPueblo* info);   // Inserta al final de la lista
+    cInfoPueblo* eliminar(std::string nombrePueblo);    // Busca elemento a eliminar
+    cInfoPueblo* buscar(std::string nombrePueblo);
+
+    int size();
+
+    bool isEmpty();
+    bool isFull();
+
+    void imprimir();
+
+    cIterador& begin();
+    cIterador& end();
+
+    void serializar(std::fstream& out);
 };
 
 #endif // CLISTA_H
