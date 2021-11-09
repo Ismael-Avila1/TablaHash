e19f9e8af61f8c2f9ad63c480f7343adf698996a
Ismael Avila <ismael.guada234015@gmail.com>
2021-11-09 12:01:34 -0600

    Definición e implementacioón del cNodo

--- cnodo.cpp
+++ cnodo.cpp
@@ -1,6 +1,33 @@
 #include "cnodo.h"
 
-cNodo::cNodo()
-{
+cNodo::cNodo() {
+    pAnt = nullptr;
+    pSig = nullptr;
+}
 
+cNodo::cNodo(cInfoPueblo* info) {
+    infoNodo = info;
+    pAnt = nullptr;
+    pSig = nullptr;
+}
+
+void cNodo::insertarAdelante(cInfoPueblo* info) {
+    cNodo* newNode = new cNodo(info);
+
+    newNode->pSig = this->pSig;
+    newNode->pAnt =this;
+
+    this->pSig = newNode;
+    this->pSig->pSig->pAnt = newNode;
+}
+
+cInfoPueblo* cNodo::eliminarAdelante() {
+    cInfoPueblo* info;
+    info = this->pSig->infoNodo;
+
+    this->pSig = this->pSig->pSig;
+    delete this->pSig->pAnt;
+    this->pSig->pAnt = this;
+
+    return info;
 }
--- cnodo.h
+++ cnodo.h
@@ -1,11 +1,22 @@
 #ifndef CNODO_H
 #define CNODO_H
 
+#include "cinfopueblo.h"
 
-class cNodo
-{
-public:
+class cNodo {
+private:
+    cInfoPueblo* infoNodo;
+    cNodo* pAnt;
+    cNodo* pSig;
+
     cNodo();
+    cNodo(cInfoPueblo* info);
+
+    void insertarAdelante(cInfoPueblo* info);
+    cInfoPueblo* eliminarAdelante();
+
+    friend class cIterador;
+    friend class cLista;
 };
 
 #endif // CNODO_H
