bb6676062626a23ae33528a4da4dec15375c9cba
Ismael Avila <ismael.guada234015@gmail.com>
2021-11-09 12:12:47 -0600

    Definición e implementacion del cIterador

--- citerador.cpp
+++ citerador.cpp
@@ -1,6 +1,43 @@
 #include "citerador.h"
 
-cIterador::cIterador()
-{
+cIterador::cIterador() {}
 
+void cIterador::setNodo(cNodo *pNodo) {
+    pActual = pNodo;
+}
+
+void cIterador::insertarAdelante(cInfoPueblo *info) {
+    pActual->insertarAdelante(info);
+}
+
+cInfoPueblo *cIterador::eliminarAdelante() {
+    return pActual->eliminarAdelante();
+}
+
+void cIterador::operator =(cIterador &otro) {
+    pActual = otro.pActual;
+}
+
+bool cIterador::operator ==(cIterador &otro) {
+    return (pActual == otro.pActual);
+}
+
+bool cIterador::operator !=(cIterador &otro) {
+    return (pActual != otro.pActual);
+}
+
+void cIterador::operator ++() {
+    pActual = pActual->pSig;
+}
+
+void cIterador::operator --() {
+    pActual = pActual->pAnt;
+}
+
+void cIterador::operator ++(int) {
+    pActual = pActual->pSig;
+}
+
+void cIterador::operator --(int) {
+    pActual = pActual->pAnt;
 }
--- citerador.h
+++ citerador.h
@@ -1,11 +1,35 @@
 #ifndef CITERADOR_H
 #define CITERADOR_H
 
+#include "cnodo.h"
 
-class cIterador
-{
+class cIterador {
+private:
+    cNodo* pActual;
+
+    void setNodo(cNodo* pNodo);
+
+    friend class cLista;
+
 public:
     cIterador();
+
+    void insertarAdelante(cInfoPueblo* info);
+    cInfoPueblo* eliminarAdelante();
+
+    // Sobrecarga de operadores
+    void operator = (cIterador& otro);
+
+    bool operator ==(cIterador& otro);
+    bool operator !=(cIterador& otro);
+
+    // Prefijos
+    void operator ++();
+    void operator --();
+
+    //Postfijos
+    void operator ++(int);
+    void operator --(int);
 };
 
 #endif // CITERADOR_H
