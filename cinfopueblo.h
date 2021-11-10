3e0e64e59a536987e04d14ebc11acf994ef9b3e7
Ismael Avila <ismael.guada234015@gmail.com>
2021-11-10 13:33:20 -0600

    Se hicieron públicos el constructor parametrizado y el método imprimir de la clase cInfoPueblo

--- cinfopueblo.h
+++ cinfopueblo.h
@@ -17,15 +17,17 @@ private:
     std::string atraccPrincipal;
 
     cInfoPueblo();
-    cInfoPueblo(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion);
-
-    void imprimir();
 
     void serializar(std::fstream& out);
 
     friend class cNodo;
     friend class cLista;
     friend class cTablaHash;
+
+public:
+    cInfoPueblo(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion);
+
+    void imprimir();
 };
 
 #endif // CINFOPUEBLO_H
